1d56ead48f4c5a280960c342a4b5ac28b1a3fb07
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-03-12 15:23:15 +0530

    Database connection and the setup the routes with database using mongoose

--- server/Routes/genre.js
+++ server/Routes/genre.js
@@ -1,63 +1,65 @@
 const express = require('express');
 const router = express.Router();
 const Joi = require('joi')
+const mongoose = require('mongoose');
 
-
-const genres = [
-    { id: 1, name: "Thriller" }, { id: 2, name: "Action" }, { id: 3, name: "Comedy" }
-]
+const Genre = mongoose.model('Genre', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50
+    }
+}))
 
 // GET : Getting all the genres
-router.get('/', (req, res) => {
-    res.send(genres);
+router.get('/', async (req, res) => {
+    const genres = await Genre.find().sort('name');
+    res.status(200).send(genres);
 })
 
 // GET : Getting the genres by their id 
-router.get('/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
+router.get('/:id', async (req, res) => {
+    const genre = await Genre.findById(req.params.id)
 
     if (!genre) return res.status(404).send('The Genre With the given id was not found');
 
-    res.send(genre);
+    res.status(200).send(genre);
 })
 
 // POST : Creating the Genre 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
 
     const { error } = validateGenre(req.body);
-    if (error) {
-        return res.status(400).send(error.details[0].message)
-    }
-    const genre = {
-        id: genres.length + 1,
-        name: req.body.name
-    }
-    genres.push(genre);
+    if (error) return res.status(400).send(error.details[0].message)
+
+    let genre = new Genre({ name: req.body.name });
+    genre = await genre.save()
+
     res.status(200).send(genre);
 })
 
 // PUT : Updating the Genre
-router.put('/:id', (req, res) => {
-
-    const genre = genres.find(g => g.id === parseInt(req.params.id))
-    if (!genre) return res.status(404).send("The Genre Withthe Given Id was not found");
+router.put('/:id', async (req, res) => {
 
     const { error } = validateGenre(req.body);
     if (error) {
         return res.status(400).send(error.details[0].message)
     }
 
-    genre.name = req.body.name;
+    const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
+        new: true
+    })
+    if (!genre) return res.status(404).send("The Genre Withthe Given Id was not found");
+
     res.status(200).send(genre);
 })
 
 // DELETE : Deleting the Genre
-router.delete('/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send('Genre With the given id was not found')
+router.delete('/:id', async (req, res) => {
+    const genre = await Genre.findByIdAndDelete(req.params.id)
 
-    const index = genres.indexOf(genre)
-    genres.splice(index, 1);
+    if (!genre) return res.status(404).send('Genre With the given id was not found')
 
     res.status(200).send(genre);
 })
--- server/index.js
+++ server/index.js
@@ -1,16 +1,25 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose')
 const genres = require('./Routes/genre')
 
 // Middlewares
-app.use(express.json()); 
+app.use(express.json());
 
 // Routes
-app.use('/api/genres',genres)
+app.use('/api/genres', genres)
 
 const port = process.env.PORT || 3000
-app.listen(port, () => {
-    console.log(`Server is Running at PORT : ${port}`)
-})
+
+
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => {
+        console.log('Database Connection Successfull')
+        app.listen(port, () => {
+            console.log(`Server is Running at PORT : ${port}`)
+        })
+    })
+    .catch((err) => {
+        console.log('Database Connection Failed :', err)
+    })
