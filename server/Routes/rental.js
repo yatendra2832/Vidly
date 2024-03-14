533d73e49f9aa6c3677fd2e8c4f88d41e05694b9
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-03-14 14:32:38 +0530

    implement the other routes for the rental api

--- server/Routes/rental.js
+++ server/Routes/rental.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router();
+const mongoose = require('mongoose');
 const { Rental, validateRental } = require('../Models/rental');
 const { Movie } = require('../Models/movie')
 const { Customer } = require('../Models/customer')
@@ -10,19 +11,19 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-    const { error } = validate(req.body)
+    const { error } = validateRental(req.body)
     if (error) return res.status(400).send(error.details[0].message)
 
-    const customer = await Customer.findById(req.body.cusotmerId);
+    const customer = await Customer.findById(req.body.customerId);
     if (!customer) return res.status(400).send("Customer With the Given id Was not Found");
 
     const movie = await Movie.findById(req.body.movieId)
-    if (movie) return res.status(400).send("Invalid Movie");
+    if (!movie) return res.status(400).send("Invalid Movie");
 
     if (movie.numberInStock === 0) return res.status(400).send('Movie Not in the Stock');
 
     let rental = new Rental({
-        cusotmer: {
+        customer: {
             _id: customer._id,
             name: customer.name,
             phone: customer.phone
@@ -35,12 +36,18 @@ router.post('/', async (req, res) => {
     })
 
     rental = await rental.save();
-
     movie.numberInStock--;
     movie.save();
 
     res.send(rental);
 
+})
+
+router.delete('/:id',async(req,res)=>{
+    const rental = await Rental.findByIdAndDelete(req.params.id);
+    if (!rental) return res.status(404).send('Rental With the given id was not found ');
+
+    res.status(200).send(rental)
 })
 
 module.exports = router;
