4d4b52815da470b1faca749dbd7b7cc685ea540e
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-03-14 21:15:22 +0530

    Implement the object id validation using the joi-objectid

--- server/Models/rental.js
+++ server/Models/rental.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi)
 
 const rentalSchema = new mongoose.Schema({
     customer: {
@@ -59,7 +60,7 @@ const Rental = mongoose.model('Rental', rentalSchema);
 
 function validateRental(rental) {
     const schema = {
-        customerId: Joi.string().required(),
+        customerId: Joi.objectId().required(),
         movieId: Joi.string().required()
     }
     return Joi.validate(rental, schema);
