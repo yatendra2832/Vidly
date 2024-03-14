b010a7382d4646c13ceb6ea86a51cad56983106f
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-03-15 00:27:27 +0530

    Implement password hashing with bcrypt in user routes

--- server/Routes/user.js
+++ server/Routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const _ = require('lodash')
+const bcrypt = require('bcrypt');
 const { User, validateUser } = require('../Models/user');
 
 router.post('/', async (req, res) => {
@@ -10,14 +11,14 @@ router.post('/', async (req, res) => {
     let user = await User.findOne({ email: req.body.email })
     if (user) return res.status(400).send("User Already Registered")
 
-    user = new User(
-        _.pick(req.body, ['name', 'email', 'password'])
-    )
+    user = new User(_.pick(req.body, ['username', 'email', 'password']))
 
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
     await user.save()
 
 
-    res.status(200).send(_.pick(user, ['_id', 'name', 'email']));
+    res.status(200).send(_.pick(user, ['_id', 'username', 'email']));
 
 
 
