098f32a815c6c86e99058ed26d762a4968f256d7
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-03-18 23:20:18 +0530

    implement the async middleware to remove the trycatch block

---
+++ server/Middleware/async.js
@@ -0,0 +1,11 @@
+function asyncMiddleware(handler) {
+    return async (req, res, next) => {
+        try {
+            await handler(req, res);
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+module.exports = {asyncMiddleware}
