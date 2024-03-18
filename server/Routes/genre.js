652ffecce3c62367a2fdad484154f8531d400f6a
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-03-18 23:22:29 +0530

    add the async middleware to the routes

--- server/Routes/genre.js
+++ server/Routes/genre.js
@@ -3,11 +3,15 @@ const router = express.Router();
 const { Genre, validateGenre } = require('../Models/genre')
 const { auth } = require('../Middleware/auth');
 const { admin } = require('../Middleware/admin');
+const { asyncMiddleware } = require('../Middleware/async');
+
 // GET : Getting all the genres
-router.get('/', async (req, res) => {
+router.get('/', asyncMiddleware(async (req, res) => {
     const genres = await Genre.find().sort('name');
     res.status(200).send(genres);
-})
+
+
+}))
 
 // GET : Getting the genres by their id 
 router.get('/:id', async (req, res) => {
