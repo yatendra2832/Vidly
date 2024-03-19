7398912755875f6e5cddab5b841e579977c1c15e
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-03-19 23:31:33 +0530

    Logging the errors

--- server/Middleware/error.js
+++ server/Middleware/error.js
@@ -1,5 +1,30 @@
+const winston = require('winston');
+const { format } = require('winston');
+
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.File({ filename: 'error.log' }), 
+  ],
+  format: format.combine(format.timestamp(), format.simple()), 
+});
+
 function error(err, req, res, next) {
-    res.status(500).send("Internal Server Error ! Something Failed")
+  
+  logger.error('Error occurred:', {
+    message: err.message,
+    stack: err.stack,
+    url: req.originalUrl, 
+    method: req.method, 
+  });
+
+  // Provide a generic error response to the client
+  res.status(500).json({
+    error: 'Internal Server Error',
+    message: 'An unexpected error occurred. Our team has been notified and is working to resolve the issue.',
+  });
+
+  next(err); 
 }
 
-module.exports = { error }
+module.exports = { error };
