1cbecc8bd11ad3fddc5748f5e21e03645c08ff31
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-03-23 15:46:57 +0530

    showing unhandled exception on the console

--- server/startup/logging.js
+++ server/startup/logging.js
@@ -5,23 +5,24 @@ require('dotenv').config();
 
 
 module.exports = function () {
-    winston.level = 'info'; 
-    winston.format = winston.format.combine(
-      winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-      winston.format.json()
-    );
-  
-    winston.handleExceptions(
-      new winston.transports.File({ filename: 'uncaughtExceptions.log' })
-    );
-  
-    process.on('unhandledRejection', (ex) => {
-      winston.error('Unhandled Promise Rejection:', ex.message);
-    });
-  
-    winston.add(winston.transports.File, { filename: 'logfile.log' });
-    winston.add(winston.transports.MongoDB, {
-      db: process.env.MONGODB_URL, 
-      collection: 'logs' 
-    });
-  };
+  // winston.level = 'info';
+  // winston.format = winston.format.combine(
+  //   winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  //   winston.format.json()
+  // );
+
+  winston.handleExceptions(
+    new winston.transports.Console({ colorize: true, prettyPrint: true }),
+    new winston.transports.File({ filename: 'uncaughtExceptions.log' })
+  );
+
+  process.on('unhandledRejection', (ex) => {
+    winston.error('Unhandled Promise Rejection:', ex.message);
+  });
+
+  winston.add(winston.transports.File, { filename: 'logfile.log' });
+  winston.add(winston.transports.MongoDB, {
+    db: process.env.MONGODB_URL,
+    collection: 'logs'
+  });
+};
