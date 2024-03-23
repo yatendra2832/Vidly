057d4d92d33a8b5230008322fe13cee10bf54af4
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-03-23 15:41:56 +0530

    refactor the validation logic

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const Joi = require('joi')
-Joi.objectId = require('joi-objectid')(Joi)
 
 require('./startup/logging')
 require('./startup/routes')(app);
 require('./startup/db')()
+require('./startup/validation')
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
